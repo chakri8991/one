dec49fa8211f112f4eee1167681bc8a879b520c1
chakri8991 <you@example.com>
2022-07-08 21:58:47 +0530

    from pc

---
+++ prac.py
@@ -0,0 +1,23 @@
+#operations on str obj
+# 1)indexing
+# 2)slicing
+# ------1)indexing
+# eg-1
+strob="chakri"
+print(strob[5])
+# ------>2)slicing
+# syntax-1)(starting index :end index)
+strob="chakri"
+print(strob[0:5])
+# syntax-2)(starting index :)
+strob="chakri"
+print(strob[0:])
+# syntax-3)(:end index)
+strob="chakri"
+print(strob[:1])
+# syntax-4)(:)
+strob="chakri"
+print(strob[:])
+# syntax-5)(begin index:end index:step)
+
+
