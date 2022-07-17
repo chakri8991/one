567a350545e1236acdd22bd9d6c0eb0918d1eac2
chakri@8991 <chakravarthi1223@gmail.com>
2022-07-17 10:53:21 +0530

    from lap

--- prac.py
+++ prac.py
@@ -3,6 +3,9 @@
 # 2)slicing
 # ------1)indexing
 # eg-1
+from msilib import sequence
+
+
 strob="chakri"
 print(strob[5])
 # ------>2)slicing
@@ -22,6 +25,38 @@ print(strob[:])
 # cahkri
 strob="chakri"
 print(strob[:])
+# print(s[0:6:2])
+# print(s[::2])
+# print(s[-6:-1:2])
+# print(s[-6::2])
+# print(s[-6:-1:2])
+# print(s[0:5:-1])
+# print(s[5:0:-1])
+# print(s[0:9:-2])
 
 
+
+#---------------type casting techniques in python
+# the purpose of type casting tec is convert one type date vlaue into another type of value
+#we have 5 type casting
+# 1)int()
+# 2)float()
+# 3)str()
+# 4)bool()
+# 5)complex()
+#-------- sequence data type
+# 2)bytes
+# b=[1,2,3,0,255]
+# b1=bytes(b)
+# print(b1,type(b1))
+# 3)bytearray
+# b=[1,2,3,0,255]
+# b1=bytearray(b)
+# print(b1,type(b1))
+# 4)range
+# r=range(0,5)
+# print(r)
+# r=range(0,5,2)
+# r=range(0,9)
+# print(r[3],type(r))
 
